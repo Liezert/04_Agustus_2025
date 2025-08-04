3452c4d2acc757548b825e1d1ef8959b9add4e71
Liezert <intazarlintangpramantanew@gmail.com>
2025-08-04 13:21:42 +0700

    perubahan nama file

---
+++ IniNamaFile.java
@@ -0,0 +1,13 @@
+public class IniNamaFile {
+    public static void main(String[] args) {
+        //main method
+        System.out.println("Intazar Lintang Pramanta");
+        System.out.println("SMK Telkom Malang");
+        System.out.println("SMP Negeri 21 Suarbaya");
+        System.out.println("Jajak Rachman");
+        System.out.println("Ita Masnawati");
+        System.out.println("IT Developer");
+        System.out.println("Bersepeda, Mendengarkan Musik");
+    }   
+}
+ 
